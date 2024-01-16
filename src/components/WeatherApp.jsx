36511ce4008f42756d5975bd62ed6214e2c902bf
dariakulish1 <dashaklsh1507@gmail.com>
2024-01-16 11:04:55 +0200

    add API in WeatherApp

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar';
 import { BottomBar } from './BottomBar';
 import { Infobar } from './Infobar';
@@ -9,6 +9,9 @@ export const WeatherApp = () => {
   const handleCityChange = (newCity) => {
     setCity(newCity);
   };
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=odesa&appid=41f68e3c835e2516f4e4e44a2aa530b2',
+  );
   return (
     <div className="App">
       <div className="divbox">
